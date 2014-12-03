de22cdc00a6ecef7a33b1bd267ba057fee3059e6
Baptiste Mikaelian <bmikaeli@debian.bmikaeli.fr>
2014-12-03 18:23:55 +0100

    ajout semantic ui

--- public/index.php
+++ public/index.php
@@ -1,14 +1,19 @@
-<?php 
+<?php
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 require_once __DIR__.'/../vendor/autoload.php';
-
 $app = new Silex\Application();
 $app['debug'] = true;
+new \Silex\Provider\TwigServiceProvider();
+$app->register(new \Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
 
-$app->get('/', function () {
-    return 'It work !';
+$app->get('/', function () use ($app){
+
+    return $app['twig']->render('layout.twig', array());
 });
 
 $app->run();
-
 ?>
