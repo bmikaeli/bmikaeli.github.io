2c682f6d3bff4bb61248e95f19c17546134cf04f
root <root@debian.bmikaeli.fr>
2014-12-03 16:04:34 +0100

    first_push

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app->get('/', function () {
+    return 'It work !';
+});
+
+$app->run();
+
+?>
