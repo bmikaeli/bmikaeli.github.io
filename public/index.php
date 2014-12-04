12c93f7e8390d3a7fbd97461c3e7149296e68d6d
root <root@c1-10-1-10-250.(none)>
2014-12-04 17:19:43 +0000

    Ajout des templates

--- public/index.php
+++ public/index.php
@@ -1,17 +1,37 @@
 <?php
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 require_once __DIR__.'/../vendor/autoload.php';
+
+
 $app = new Silex\Application();
 $app['debug'] = true;
+
 new \Silex\Provider\TwigServiceProvider();
+
 $app->register(new \Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 
-$app->get('/', function () use ($app){
-    return $app['twig']->render('layout.twig', array());
+$contact = $app['controllers_factory'];
+$contact->get('/', function () use ($app){
+    return $app['twig']->render('contact.twig', array(
+        'tab' => 'contact',
+    ));
 });
 
+$skills = $app['controllers_factory'];
+$skills->get('/', function () use ($app){
+    return $app['twig']->render('skills.twig', array(
+        'tab' => 'skills',
+    ));
+});
+
+$app->get('/', function () use ($app){
+    return $app['twig']->render('index.twig', array(
+        'tab' => 'index',
+    ));
+});
+
+$app->mount('/contact', $contact);
+$app->mount('/skills', $skills);
 $app->run();
 ?>
