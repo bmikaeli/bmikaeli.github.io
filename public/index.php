0fd600e27a13e2c1bf142332b20a4ea7562b9985
root <root@c1-10-1-10-250.(none)>
2014-12-05 14:27:14 +0000

    Ajout de pop-up

--- public/index.php
+++ public/index.php
@@ -10,9 +10,37 @@ new \Silex\Provider\TwigServiceProvider();
 $app->register(new \Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
+$app->register(new Silex\Provider\SwiftmailerServiceProvider());
+
 
 $contact = $app['controllers_factory'];
 $contact->get('/', function () use ($app){
+    return $app['twig']->render('contact.twig', array(
+        'tab' => 'contact',
+    ));
+});
+
+$contact->post('/', function (\Symfony\Component\HttpFoundation\Request $request) use ($app){
+    $prenom = $request->get('prenom');
+    $message = $request->get('message');
+    $email = $request->get('email');
+    $sujet = $request->get('sujet');
+    $nom = $request->get('nom');
+
+    $transport = Swift_SmtpTransport::newInstance(
+        //smtp configuration here
+    );
+
+    $mailer = Swift_Mailer::newInstance($transport);
+
+    $mail = \Swift_Message::newInstance()
+        ->setSubject($sujet)
+        ->setFrom(array('noreply@yoursite.com' => $prenom . " ". $nom))
+        ->setTo(array('baptiste.mikaelian@gmail.com'))
+        ->setBody($message);
+
+    $result = $mailer->send($mail, $echec);
+
     return $app['twig']->render('contact.twig', array(
         'tab' => 'contact',
     ));
---
+++ public/scripts/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by bmikaeli on 12/5/2014.
+ */
+$(document).ready(function ()
+{
+    $(".popup").popup();
+    console.info('pop-up initialized');
+});
